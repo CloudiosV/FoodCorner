0d53f5caf7980fc566ed54cbbd9d9983445722b4
CloudiosV <cloudiosss@gmail.com>
2026-02-23 16:43:45 +0700

    feat: upload image

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,14 +1,35 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import type { Product } from "@prisma/client";
+import { writeFile } from "fs/promises";
+import path from "path";
 
 export const POST = async (request: Request) => {
-    const body: Product = await request.json();
+    const data = await request.formData();
+
+    const name = data.get("name") as string;
+    const price = Number(data.get("price"));
+    const category_id = Number(data.get("category_id"));
+    const file = data.get("image") as File;
+
+    let fileName = "";
+
+    if (file) {
+        const bytes = await file.arrayBuffer();
+        const buffer = Buffer.from(bytes);
+
+        fileName = Date.now() + "-" + file.name;
+
+        const uploadPath = path.join(process.cwd(), "public/uploads", fileName);
+        await writeFile(uploadPath, buffer);
+    }
+
     const product = await prisma.product.create({
         data:{
-            name: body.name,
-            price: body.price,
-            category_id: body.category_id
+            name,
+            price,
+            category_id,
+            image: fileName
         }
     });
 
