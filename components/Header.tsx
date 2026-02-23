41655a6b8d76b265a64b9e640fe534e164f87b32
CloudiosV <cloudiosss@gmail.com>
2026-02-23 16:43:00 +0700

    feat: header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,22 +7,24 @@ import { signOut } from "next-auth/react";
 export default function Header() {
   const pathname = usePathname();
   
-  if (pathname.startsWith("/dashboard") || pathname.startsWith("/login")) {
+  if (pathname == "/" || pathname.startsWith("/dashboard") || pathname.startsWith("/login")) {
     return null;
   }
 
   const handleLogout = () => {
-    signOut({callbackUrl: "/login"});
+    signOut({callbackUrl: "/"});
   }
 
   return (
     <header className="bg-black text-white p-4 flex gap-6">
-      <div className="flex gap-4">
-        <Link href="/sales">Sales</Link>
-        <Link href="/products">Products</Link>
-        <Link href="/category">Category</Link>
-        <Link href="/promo">Promo</Link>
-      </div>
+      {!pathname.startsWith("/orders") && (
+        <div className="flex gap-4">
+          <Link href="/sales">Sales</Link>
+          <Link href="/products">Products</Link>
+          <Link href="/category">Category</Link>
+          <Link href="/promo">Promo</Link>
+        </div>
+      )}
 
       <button onClick={handleLogout} className="ml-auto text-red-400">Logout</button>
     </header>
