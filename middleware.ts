5ee35464cd9e7d05e82b7f9802b6bc2e6de85333
CloudiosV <cloudiosss@gmail.com>
2026-04-14 15:32:15 +0700

    fix: update deprecated middleware to use proxy

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,11 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+// Kita bungkus secara eksplisit agar Next.js mengenali ini sebagai fungsi export
+export default withAuth({
+  pages: {
+    signIn: "/login", // Arahkan ke halaman login kamu jika belum auth
+  },
+});
 
 export const config = {
   matcher: [
