ec65d2e71c138b9943b58e0412698cb098d23e77
CloudiosV <cloudiosss@gmail.com>
2026-04-14 14:16:04 +0700

    refactor: make function asynchronous with Promise

--- app/api/sales/[id]/route.ts
+++ app/api/sales/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-export const GET = async (request: Request, context: {params: {id: string}}) => {
+export const GET = async (request: Request, context: {params: Promise<{id: string}>}) => {
     const { id } = await context.params;
     const sales = await prisma.sales.findUnique({
         where: {
